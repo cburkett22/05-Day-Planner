76c9a69e4cff873a39864b1ae4f6a49c86f538ce
Corey Burkett <coreyburkett22@gmail.com>
2020-07-10 14:54:03 -0700

    Created current date

---
+++ Develop/script.js
@@ -0,0 +1,33 @@
+// Current Date
+$(document).ready(function currentDate() {
+let month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+let day = new Array();
+    day[0] = "Monday";
+    day[1] = "Tuesday";
+    day[2] = "Wednesday";
+    day[3] = "Thursday";
+    day[4] = "Friday";
+    day[5] = "Saturday";
+    day[6] = "Sunday";
+    
+let d = new Date();
+let dayNum = d.getDate();
+let output = day[d.getDay() -1] + ', ' + month[d.getMonth()] + ' ' + dayNum + 'th';
+
+$('#currentDay').html(output);
+});
+
+
